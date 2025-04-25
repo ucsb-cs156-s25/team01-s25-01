6397fbb3e3dfb79bf0150e6e91925e9f0d9f901c
ranchen <ranchen@ucsb.edu>
2025-04-25 14:06:03 -0700

    add article controller tests

---
+++ src/test/java/edu/ucsb/cs156/example/controllers/ArticleControllerTests.java
@@ -0,0 +1,136 @@
+package edu.ucsb.cs156.example.controllers;
+
+import edu.ucsb.cs156.example.repositories.UserRepository;
+import edu.ucsb.cs156.example.testconfig.TestConfig;
+import edu.ucsb.cs156.example.ControllerTestCase;
+import edu.ucsb.cs156.example.entities.Article;
+import edu.ucsb.cs156.example.entities.UCSBDate;
+import edu.ucsb.cs156.example.repositories.ArticlesRepository;
+import edu.ucsb.cs156.example.repositories.UCSBDateRepository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(controllers = ArticleController.class)
+@Import(TestConfig.class)
+public class ArticleControllerTests extends ControllerTestCase{
+    @MockitoBean
+    ArticlesRepository articlesRepository;
+
+    @Test
+    public void logged_out_users_cannot_get_all() throws Exception {
+            mockMvc.perform(get("/api/articles/all"))
+                            .andExpect(status().is(403)); // logged out users can't get all
+    }
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void logged_in_users_can_get_all() throws Exception {
+            mockMvc.perform(get("/api/articles/all"))
+                            .andExpect(status().is(200)); // logged
+    }
+    @Test
+    public void logged_out_users_cannot_post() throws Exception {
+            mockMvc.perform(post("/api/articles/post"))
+                            .andExpect(status().is(403));
+    }
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void logged_in_regular_users_cannot_post() throws Exception {
+            mockMvc.perform(post("/api/articles/post"))
+                            .andExpect(status().is(403)); // only admins can post
+    }
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void logged_in_user_can_get_all_ucsbdates() throws Exception {
+
+            // arrange
+            ZonedDateTime zdt1 = ZonedDateTime.parse("2025-04-25T00:30:19.455Z");
+
+            Article article1 = Article.builder()
+                .title("test_title")
+                .url("https://github.com/ucsb-cs156-s25/team01-s25-01")
+                .explanation("test repo")
+                .email("ranchen@ucsb.edu")
+                .dateAdded(zdt1)
+                .build();
+
+
+            ArrayList<Article> expectedArticles = new ArrayList<>();
+            expectedArticles.add(article1);
+
+            when(articlesRepository.findAll()).thenReturn(expectedArticles);
+
+            // act
+            MvcResult response = mockMvc.perform(get("/api/articles/all"))
+                            .andExpect(status().isOk()).andReturn();
+
+            // assert
+
+            verify(articlesRepository, times(1)).findAll();
+            String expectedJson = mapper.writeValueAsString(expectedArticles);
+            String responseString = response.getResponse().getContentAsString();
+            assertEquals(expectedJson, responseString);
+    }
+
+    @WithMockUser(roles = { "ADMIN", "USER" })
+    @Test
+    public void an_admin_user_can_post_a_new_ucsbdate() throws Exception {
+            // arrange
+
+            ZonedDateTime zdt1 = ZonedDateTime.parse("2025-04-25T00:30:19.455Z");
+            Article article1 = Article.builder()
+                                .title("test_title")
+                                .url("https://github.com/ucsb-cs156-s25/team01-s25-01")
+                                .explanation("test repo")
+                                .email("ranchen@ucsb.edu")
+                                .dateAdded(zdt1)
+                                .build();
+           
+
+            when(articlesRepository.save(eq(article1))).thenReturn(article1);
+
+            // act
+            MvcResult response = mockMvc.perform(
+                            post("/api/articles/post?name=firstDayOfClasses&quarterYYYYQ=20222&localDateTime=2022-01-03T00:00:00")
+                                            .with(csrf()))
+                            .andExpect(status().isOk()).andReturn();
+
+            // assert
+            verify(articlesRepository, times(1)).save(article1);
+            String expectedJson = mapper.writeValueAsString(article1);
+            String responseString = response.getResponse().getContentAsString();
+            assertEquals(expectedJson, responseString);
+    }
+
+}
