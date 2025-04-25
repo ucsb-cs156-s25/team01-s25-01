e35d623de8b80f4ee2be691b1db72e8927316d71
Oscar Luna <oscar_luna@uscb.edu>
2025-04-24 21:20:11 -0700

    creates MenuItemReview Controller

--- src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
+++ src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "MenuItemReviews")
+@Entity(name = "menuitemreview")
 public class MenuItemReview {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
