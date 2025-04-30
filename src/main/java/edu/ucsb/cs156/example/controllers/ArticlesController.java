f74d956c8134439ffc5bbce47f758683a09c1775
ranchen <ranchen@ucsb.edu>
2025-04-29 17:44:59 -0700

    edit controller comment to fit zoned time

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -70,7 +70,7 @@ public class ArticlesController extends ApiController {
             @Parameter(name = "url") @RequestParam String url,
             @Parameter(name = "explanation") @RequestParam String explanation,
             @Parameter(name = "email") @RequestParam String email,
-            @Parameter(name = "dateAdded", description = "date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("dateAdded") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime dateAdded)
+            @Parameter(name = "dateAdded", description = "date (in iso format, e.g. YYYY-mm-ddTHH:MM:SSZ)") @RequestParam("dateAdded") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime dateAdded)
             throws JsonProcessingException {
 
         // For an explanation of @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
